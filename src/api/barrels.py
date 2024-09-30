7339b558b8dd2c04ccdb76d9ec53c4523b837d53
joelbravo01 <joelbravo027@gmail.com>
2024-09-30 20:49:13 +0000

    edit

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -2,6 +2,9 @@ from fastapi import APIRouter, Depends
 from pydantic import BaseModel
 from src.api import auth
 
+import sqlalchemy
+from src import database as db
+
 router = APIRouter(
     prefix="/barrels",
     tags=["barrels"],
@@ -19,7 +22,10 @@ class Barrel(BaseModel):
 
 @router.post("/deliver/{order_id}")
 def post_deliver_barrels(barrels_delivered: list[Barrel], order_id: int):
-    """ """
+    
+    with db.engine.begin() as connection:
+        result = connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_green_potions = num_green_ml + 1"))
+
     print(f"barrels delievered: {barrels_delivered} order_id: {order_id}")
 
     return "OK"
@@ -27,12 +33,20 @@ def post_deliver_barrels(barrels_delivered: list[Barrel], order_id: int):
 # Gets called once a day
 @router.post("/plan")
 def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
-    """ """
+    with db.engine.begin() as connection:
+        result = connection.execute(sqlalchemy.text("SELECT num_green_potions FROM global_inventory"))
     print(wholesale_catalog)
 
-    return [
+    rrow = result.fetchone()
+
+    if row:
+        greenpotionsinventory = row("num_green_potions")
+        print(greenpotionsinventory)
+
+    if greenpotionsinventory < 10:
+        return [
         {
-            "sku": "SMALL_RED_BARREL",
+            "sku": "SMALL_GREEN_BARREL",
             "quantity": 1,
         }
     ]
--- src/api/bottler.py
+++ src/api/bottler.py
@@ -3,6 +3,9 @@ from enum import Enum
 from pydantic import BaseModel
 from src.api import auth
 
+import sqlalchemy
+from src import database as db
+
 router = APIRouter(
     prefix="/bottler",
     tags=["bottler"],
@@ -16,28 +19,33 @@ class PotionInventory(BaseModel):
 @router.post("/deliver/{order_id}")
 def post_deliver_bottles(potions_delivered: list[PotionInventory], order_id: int):
     """ """
+
+    with db.engine.begin() as connection:
+        result = connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_green_ml = num_green_ml + 1"))
+
     print(f"potions delievered: {potions_delivered} order_id: {order_id}")
 
     return "OK"
 
 @router.post("/plan")
 def get_bottle_plan():
-    """
-    Go from barrel to bottle.
-    """
-
+    with db.engine.begin() as connection:
+        result = connection.execute(sqlalchemy.text("SELECT num_green_ml FROM global_inventory"))
     # Each bottle has a quantity of what proportion of red, blue, and
     # green potion to add.
     # Expressed in integers from 1 to 100 that must sum up to 100.
 
     # Initial logic: bottle all barrels into red potions.
-
-    return [
+    row = result.fetchone()
+    if row:
+        greenpotionml = row["num_green_ml"]
+        print(greenpotionml)
+    if greenpotionml > 0:
+        return [
             {
-                "potion_type": [100, 0, 0, 0],
+                "potion_type": [0, 100, 0, 0],
                 "quantity": 5,
             }
         ]
-
 if __name__ == "__main__":
     print(get_bottle_plan())
